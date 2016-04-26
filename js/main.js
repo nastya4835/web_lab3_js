5fdf159967cda026374aafb9afbf36d6fb770ebc
Anastasiya <shapovalova4835@gmail.com>
2016-04-26 22:48:36 +0400

    email

--- js/main.js
+++ js/main.js
@@ -27,6 +27,25 @@ $(function(){
         });
 
 
+$(document).ready(function() {
+    $('#email').blur(function() {
+        if($(this).val() != '') {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if(pattern.test($(this).val())){
+                $(this).css({'border' : '1px solid #569b44'});
+                $('#valid').text('Верно');
+            } else {
+                $(this).css({'border' : '1px solid #ff0000'});
+                $('#valid').text('Не верно');
+            }
+        } else {
+            $(this).css({'border' : '1px solid #ff0000'});
+            $('#valid').text('Поле email не должно быть пустым');
+        }
+    });
+});
+
+
 
 
         // $button.click(function() {
